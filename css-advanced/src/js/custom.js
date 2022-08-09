bfea5cf806ef034e4fe8da05169f0630f9a0a211
Abraão Albuquerque <abraao.hs2@gmail.com>
2022-08-09 12:37:01 -0300

    adicionando modal com formularios e estilizacao

---
+++ css-advanced/src/js/custom.js
@@ -0,0 +1,8 @@
+const button = document.getElementsByClassName('modal__body__forms__button');
+const modal = document.getElementsByClassName('modal');
+
+console.log(modal);
+
+if(modal.style.display==='flex'){
+  modal.style.display='none';
+}
