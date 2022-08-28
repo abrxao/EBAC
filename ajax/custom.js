0a21738a346dd353d92328626810f3fa81f39dac
Abraão Albuquerque <abraao.hs2@gmail.com>
2022-08-28 01:14:03 -0300

    removendo bug do doubleclick

--- ajax/custom.js
+++ ajax/custom.js
@@ -32,48 +32,52 @@ fetch(url_reqes)
         
         if(perfil.id==1){
             perfis.innerHTML += `<div class="perfil__destack"id="${perfil.id}">
-                <div class="perfil__disable">
-                </div><div class="perfil__window">
-                </div><div class= "perfil__info">
-                    <h3>${perfil.first_name} ${perfil.last_name}</h3>
-                    <img src="${perfil.avatar}"></img>
-                    <p>E-mail: '${perfil.email}'</p>
-                </div>
+            <div class="perfil__disable">
+            </div><div class="perfil__window">
+            </div><div class= "perfil__info">
+            <h3>${perfil.first_name} ${perfil.last_name}</h3>
+            <img src="${perfil.avatar}"></img>
+            <p>E-mail: '${perfil.email}'</p>
+            </div>
             </div>`; 
         }else{
             perfis.innerHTML += `<div class="perfil__card" id="${perfil.id}">
-                <div class="perfil__window"></div>
-                <div class= "perfil__info">
-                    <h3>${perfil.first_name} ${perfil.last_name}</h3>
-                    <img src="${perfil.avatar}"></img>
-                    <p>E-mail: '${perfil.email}'</p>
-                </div>
+            <div class="perfil__window"></div>
+            <div class= "perfil__info">
+            <h3>${perfil.first_name} ${perfil.last_name}</h3>
+            <img src="${perfil.avatar}"></img>
+            <p>E-mail: '${perfil.email}'</p>
+            </div>
             </div>`; 
         }
         
     });
-
+    
     var cardAux = '';
     cards =document.querySelectorAll('.perfil__card');
     cards.forEach(card=>{
         card.addEventListener('click', e =>{
+            cards.forEach(c=>{c.classList.add('noClick')})
+
             var destack = document.querySelector('.perfil__destack');
             var wdwCard = card.querySelector('.perfil__window');
             var wdwDestack = destack.querySelector('.perfil__window');
             cardAux = destack.querySelector('.perfil__info').innerHTML;
-
-            wdwDestack.setAttribute('style','animation: .7s switchEnd ease-in-out');
-            wdwCard.setAttribute('style','animation: .7s switchRot ease-in-out');
-
+            
+            wdwDestack.setAttribute('style','animation: .8s switchEnd ease-in-out');
+            wdwCard.setAttribute('style','animation: .8s switchRot ease-in-out');            
+            
             setTimeout(()=>{
+                dbcAux= card.querySelector('.perfil__info').innerHTML;
                 destack.querySelector('.perfil__info').innerHTML=card.querySelector('.perfil__info').innerHTML;
                 card.querySelector('.perfil__info').innerHTML=cardAux;
-            },350)
+            },400)
             setTimeout(()=>{
                 wdwCard.setAttribute('style','animation:none')
                 wdwDestack.setAttribute('style',"animation:none");
-                
-            },720) 
+                cards.forEach(c=>{c.classList.remove('noClick')})
+            },800)
+            
         });
     })
 })
