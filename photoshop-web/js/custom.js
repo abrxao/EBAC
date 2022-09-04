970d1648ba9b5689200659658f65d7fde8e347f1
Abraão Albuquerque <abraao.hs2@gmail.com>
2022-09-04 18:27:21 -0300

    copiando layout a partir de projeto no photoshop

---
+++ photoshop-web/js/custom.js
@@ -0,0 +1,11 @@
+const controlButtons = document.querySelectorAll('.banner__slide__controlButtons button');
+const slide = document.querySelector('.banner__slide__info');
+
+controlButtons.forEach((b)=>{
+    b.addEventListener('click', ()=>{
+        if(b.getAttribute('id')=='bt1'){
+            
+        }
+        
+    });
+});
