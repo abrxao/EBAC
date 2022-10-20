d9984824d63b8816a5f24d93c222fd420fa72b2f
Abraão Albuquerque <abraao.hs2@gmail.com>
2022-10-20 19:09:59 -0300

    adicionando codigo para pedir e atualizar mapa com geolocation

---
+++ API/src/js/custom.js
@@ -0,0 +1,16 @@
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showLocation);
+}
+
+function showLocation(position){
+    const blockMaps = document.querySelector("#blockMaps");
+    const mapsTitle = document.querySelector(".loc__maps__title");
+    const long = position.coords.longitude;
+    const lat = position.coords.latitude;
+
+    const url = `https://maps.google.com/maps?q=${lat},${long}&t=&z=16&ie=UTF8&iwloc=&output=embed`;
+
+    blockMaps.setAttribute('src', url);
+    mapsTitle.parentNode.removeChild(mapsTitle);
+    blockMaps.style.display = 'block';
+}
